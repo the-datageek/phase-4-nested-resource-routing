c14a3266e41b8dfa70be2a05a57039344c00e669
the-datageek <moraaf770@gmail.com>
2023-03-31 10:05:58 +0300

    Done

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,7 +1,7 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def show
+ def show
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
   end
@@ -11,5 +11,4 @@ class DogHousesController < ApplicationController
   def render_not_found_response
     render json: { error: "Dog house not found" }, status: :not_found
   end
-
 end
